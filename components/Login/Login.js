b53ed8ef7244a709511d9e1dc18c2cdfaf640226
Golam Mursalin <golammursalin309@gmail.com>
2023-06-11 10:33:15 +0600

    email validation added

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,10 +1,36 @@
 import React, { useState } from "react";
 import styles from "./Login.module.css";
+import { useRouter } from "next/router";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const [userMsg, setUserMsg] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
 
+  // Read Email from Input Field
+  const handleEnteredEmail = (e) => {
+    setError("");
+    setEmail(e.target.value);
+  };
+
+  //Handle SignIn
+  const handleSignIn = (e) => {
+    e.preventDefault();
+
+    // Validate Email
+    if (
+      !email ||
+      !/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-]+)(\.[a-zA-Z]{2,5}){1,2}$/.test(
+        email
+      )
+    ) {
+      setError("Enter a valid email address");
+      return;
+    }
+
+    router.push("/");
+  };
   return (
     <div className={styles.main}>
       <div className={styles.mainWrapper}>
@@ -14,10 +40,11 @@ const Login = () => {
           type="text"
           placeholder="Email address"
           className={styles.emailInput}
+          onChange={handleEnteredEmail}
         />
 
-        <p className={styles.userMsg}>{userMsg}</p>
-        <button className={styles.loginBtn}>
+        <p className={styles.error}>{error}</p>
+        <button onClick={handleSignIn} className={styles.loginBtn}>
           {isLoading ? "Loading..." : "Sign In"}
         </button>
       </div>
